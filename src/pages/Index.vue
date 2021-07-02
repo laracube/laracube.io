0255400e3d89d261bf6f411c0de0fc5a836c2a9a
Akki Khare <admin@akki.io>
2021-07-02 00:56:37 -0400

    add CTA

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,6 +4,7 @@
         <screenshot></screenshot>
         <tech-stack></tech-stack>
         <features></features>
+        <cta-comp></cta-comp>
     </Layout>
 </template>
 
@@ -12,8 +13,9 @@ import Hero from '~/components/home/Hero';
 import Screenshot from '~/components/home/Screenshot';
 import TechStack from '~/components/home/TechStack';
 import Features from '~/components/home/Features';
+import CtaComp from '~/components/home/Cta';
 export default {
-    components: { Features, TechStack, Screenshot, Hero },
+    components: { Features, TechStack, Screenshot, Hero, CtaComp },
     metaInfo: {
         title: 'Home',
     },
